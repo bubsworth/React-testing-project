d2f70b5c9dfe33c4277c0054a00c371b045b310b
bubsworth <bubbard96@hotmail.com>
2024-01-29 15:53:06 +0000

    Working With Mocks

--- src/components/Async.js
+++ src/components/Async.js
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const Async = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((data) => {
         setPosts(data);
--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -2,10 +2,15 @@ import { render, screen } from "@testing-library/react";
 import Async from "./Async";
 
 describe("Async component", () => {
-  test("renders posts if request succeeds", () => {
+  test("renders posts if request succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First Post" }],
+    });
+
     render(<Async />);
 
-    const listItemElements = screen.findAllByRole("listitem");
+    const listItemElements = await screen.findAllByRole("listitem");
     expect(listItemElements).not.toHaveLength(0);
   });
 
